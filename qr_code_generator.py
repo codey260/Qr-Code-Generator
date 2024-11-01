e6a00e3d311957390d26cf8b008727de121e7d73
Youssef Shaaban <yousafsh351@gmail.com>
2024-11-01 14:21:33 +0200

    Edited Project File

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -4,10 +4,10 @@ import png
 from pyqrcode import QRCode
 
 # Declaring The Variable Which Represents To Url, Text Or Phone Number
-text = "https://www.github.com/youcefshaaban"
+text = input("Type In URL or Text: ").strip()
 
 # Making The Qr Code
 qrcode = pyqrcode.create(text)
 
 # Saving The QrCode In File Named "MyQrcode.png"
-qrcode.png("MyQrcode.png", scale=100)
+qrcode.png("MyQrcode.png", scale=20)
